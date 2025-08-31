45a53fe965c1da3b28a3b8a0c52d2d7246078a08
Freakbomb08 <ayushjaiwal6392@gmal.com>
2025-08-31 22:12:27 +0530

    Add Pages

---
+++ src/pages/Index.tsx
@@ -0,0 +1,29 @@
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import ChatInterface from "@/components/ChatInterface";
+import OceanMap from "@/components/OceanMap";
+import TechStack from "@/components/TechStack";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Navigation />
+      <main>
+        <HeroSection />
+        <div id="chat">
+          <ChatInterface />
+        </div>
+        <div id="map">
+          <OceanMap />
+        </div>
+        <div id="tech">
+          <TechStack />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
